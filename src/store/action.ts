78dc696faad3c8f3289391072620f1dbfdd9dd4e
HoweelZhang <276001697@qq.com>
2018-11-15 17:28:09 +0800

    添加store 数据层

---
+++ src/store/action.ts
@@ -0,0 +1,9 @@
+// import * as Vuex from 'vuex';
+
+// type Context = Vuex.ActionContext<State, State>
+
+// const actions = {
+//     getActivePage(context: Context, loggedInState: boolean) {
+//         commitLoggedInState(context,loggedInState)
+//     }
+// }
